a18dc16b1b77fab62d13301306b98588c07f5904
Timothy Studd <timstudd@gmail.com>
2024-10-29 10:36:51 -0500

    Fix connection setting key

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const connection = mysql.createConnection({
   database: process.env.DB_NAME,
-  username: process.env.DB_USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
